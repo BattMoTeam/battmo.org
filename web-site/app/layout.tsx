a01553d2ef5c028ab99627f6b6928d8b51fdcf9c
Lorena Hendrix <lorenahendrix163@gmail.com>
2025-10-22 13:36:39 +0200

    correct favicon

--- web-site/app/layout.tsx
+++ web-site/app/layout.tsx
@@ -11,13 +11,13 @@ export default function RootLayout({ children }: { children: ReactNode }) {
         <title>My Website</title>
 
         {/* Favicon */}
-        <link rel="icon" href="/battmo_logo_thumb.svg" />
+        <link rel="icon" href="battmo_logo_thumb.svg" />
         {/* Optional: multiple sizes */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        {/* <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         {/* <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" /> */}
 
         {/* Optional: Apple touch icon */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        {/* <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" /> */} 
       </head>
 
       <body>
